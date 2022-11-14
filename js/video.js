156cb0ac4452197b9a9e29f5995135a719e47b1a
jryzhkov3 <jryzhkov3@gatech.edu>
2022-11-14 15:43:44 -0500

    Initial Push

---
+++ js/video.js
@@ -0,0 +1,15 @@
+
+/*=============================================
+	=    		Magnific Popup		      =
+=============================================*/
+$('.popup-image').magnificPopup({
+	type: 'image',
+	gallery: {
+		enabled: true
+	}
+});
+
+/* magnificPopup video view */
+$('.popup-video').magnificPopup({
+	type: 'iframe'
+});
